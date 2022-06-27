cec69bf382eacb138c1557a3b3168d44db1be2f4
com <cocaomouse@gmail.com>
2022-06-27 10:12:34 +0000

    管理后台 - 新增、编辑商品

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -8,12 +8,16 @@ use App\Admin\Controllers\ProductsController;
 Admin::routes();
 
 Route::group([
-    'prefix'        => config('admin.route.prefix'),
-    'namespace'     => config('admin.route.namespace'),
-    'middleware'    => config('admin.route.middleware'),
+    'prefix' => config('admin.route.prefix'),
+    'namespace' => config('admin.route.namespace'),
+    'middleware' => config('admin.route.middleware'),
     //'as'            => config('admin.route.prefix') . '.',
 ], function (Router $router) {
-    $router->get('/', [HomeController::class,'index'])->name('home');
-    $router->get('users',[UsersController::class,'index']);
-    $router->get('products',[ProductsController::class,'index']);
+    $router->get('/', [HomeController::class, 'index'])->name('home');
+    $router->get('users', [UsersController::class, 'index']);
+    $router->get('products', [ProductsController::class, 'index']);
+    $router->get('products/create', [ProductsController::class, 'create']);
+    $router->post('products', [ProductsController::class, 'store']);
+    $router->get('products/{id}/edit',[ProductsController::class,'edit']);
+    $router->put('products/{id}',[ProductsController::class,'update']);
 });
