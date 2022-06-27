3a82d2ffedcf9e56aea7594dd23a5854b6b4e2ad
com <cocaomouse@gmail.com>
2022-06-27 02:59:49 +0000

    管理后台 - 用户列表

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Routing\Router;
+use App\Admin\Controllers\HomeController;
+use App\Admin\Controllers\UsersController;
 
 Admin::routes();
 
@@ -8,9 +10,8 @@ Route::group([
     'prefix'        => config('admin.route.prefix'),
     'namespace'     => config('admin.route.namespace'),
     'middleware'    => config('admin.route.middleware'),
-    'as'            => config('admin.route.prefix') . '.',
+    //'as'            => config('admin.route.prefix') . '.',
 ], function (Router $router) {
-
-    $router->get('/', 'HomeController@index')->name('home');
-
+    $router->get('/', [HomeController::class,'index'])->name('home');
+    $router->get('users',[UsersController::class,'index']);
 });
