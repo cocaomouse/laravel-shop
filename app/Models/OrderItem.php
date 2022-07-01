d9519df5159728568059a9150df70f7cae901787
com <cocaomouse@gmail.com>
2022-07-01 02:28:16 +0000

    订单模型

---
+++ app/Models/OrderItem.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OrderItem extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'amount',
+        'price',
+        'rating',
+        'review',
+        'reviewed_at'
+    ];
+
+    protected $dates = ['reviewed_at'];
+
+    //public $timestamps = false;
+
+    /*--------------------关联关系-------------------------*/
+    public function product()
+    {
+        return $this->belongsTo('App\Models\Product','product_id','id');
+    }
+
+    public function productSku()
+    {
+        return $this->belongsTo('App\Models\ProductSku','product_sku_id','id');
+    }
+
+    public function order()
+    {
+        return $this->belongsTo('App\Models\Order','order_id','id');
+    }
+}
